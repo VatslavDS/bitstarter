1401449fffa39e15fb3323798e0fa827cd35fb2c
VatslavDS <gardna_big@hotmail.com>
2013-07-20 05:37:57 +0000

    se corrigio el web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  	var content = fs.readFileSync('index.html', 'utf8');
+  	var content = fs.readFileSync('index.html', 'text/html');
 	response.send(content);
 });
 
