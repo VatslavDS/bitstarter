0c39062e7e17d404b0654793e913cdfb77f48a93
VatslavDS <gardna_big@hotmail.com>
2013-08-04 02:48:26 +0000

    Mejoramos el index y le metimos botones de redes sociales

--- web.js
+++ web.js
@@ -1,7 +1,6 @@
 var express = require('express');
-var app = express();
+var app = express.createServer(express.logger());
 var fs = require('fs');
-app.use(express.logger());
 
 app.get('/', function(request, response) {
     var content = fs.readFileSync('index.html', 'utf8');
